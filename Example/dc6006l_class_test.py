aa0ddfac14263e9eddba7498a72aad2e3d61894d
Aleksandr Ahasnyk <alexandr.chasnyk@yandex.com>
2021-04-02 11:55:56 +0300

    Update descriptions
    Update descriptions
    added to example sys lib to use /src folder

--- Example/dc6006l_class_test.py
+++ Example/dc6006l_class_test.py
@@ -1,21 +1,29 @@
+
+
+import sys
+#need this string to be able import the lib from /src folder
+#otherwise put  dc6006l_class.py at root of your project
+sys.path.append('../src')
 import dc6006l_class as ps
 import time
 
-power = ps.dc6006l_class()
-result = power.init("COM5")
-power.get_status()
-power.output_enable()
-power.set_v_out(13.55)
-for v in range(10000):
-    time.sleep(1)
-    state = power.get_state()
-    current = state[1]
-    print(f"Current:{state[1]}, Voltage: {state[0]}")
-    if current == 0:
-        print(f"ps restart")
-        power.output_disable()
-        time.sleep(5)
-        power.output_enable()
+if __name__ == "__main__":
 
+    power = ps.dc6006l_class()
+    result = power.init("COM5")
+    power.get_status()
+    power.output_enable()
+    power.set_v_out(13.55)
+    for v in range(10000):
+        time.sleep(1)
+        state = power.get_state()
+        print(f"Current:{state[1]}, Voltage: {state[0]}")
+        current = state[1]
+        if current == 0:
+            print(f"ps restart")
+            power.output_disable()
+            time.sleep(5)
+            power.output_enable()
 
-power.close()
+    power.output_disable()
+    power.close()
