3b6a1546cd98871ac0ca93cfcb2cadecd970a585
ami3go <69671996+ami3go@users.noreply.github.com>
2021-04-07 11:03:43 +0300

    corrected and clean up

--- Example/Meander Generation.py
+++ Example/Meander Generation.py
@@ -5,31 +5,38 @@ import src.dc6006l_class as ps
 #import dc6006l_class as ps
 import time
 
+
+
+
+
 if __name__ == "__main__":
+    #variables
+    Vout_high = 5  # voltage of high level, here 5V
+    Nreps = 10
+    on_pulse_width = 0.5  # time in second
+    off_pulse_width = 0.8  # time in seconds
+
+
 
     power = ps.dc6006l_class()
     result = power.init("COM4")
     power.get_status()
     power.enable_state_reporting()
     power.output_disable()
-    power.set_v_out(0.01)
+    power.set_v_out(Vout_high)
     power.output_enable()
 
-    vout = 0.00
-    voltage_step = 0.05
-    Vout_high = 5
-    Nreps = 10
-    on_pulse_width = 1
-    off_pulse_width = 1
+
+
     power.set_v_out(Vout_high)
     for x in range(0, Nreps):
         power.output_enable()
         time.sleep(on_pulse_width)
-        print(f"Vout: {vout} State: {power.get_state()}")
+        print(f" OnTime:{on_pulse_width} s, State: {power.get_state()}")
 
         power.output_disable()
         time.sleep(off_pulse_width)
-        print(f"Vout: {vout} State: {power.get_state()}")
+        print(f"OffTime:{off_pulse_width} s, State: {power.get_state()}")
 
 
     power.output_disable()
